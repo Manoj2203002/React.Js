3ce15ce3725cb0016af7b44d02743e42af97d04a
Manoj2203002 <manoj220302@gmail.com>
2024-12-24 20:14:27 +0530

    Mynotes

--- react/src/Useeffect/Useeffect.jsx
+++ react/src/Useeffect/Useeffect.jsx
@@ -4,14 +4,18 @@ const Useeffect = () => {
     const[value,setvalue]=useState(0)
     const[data,setData]=useState("");
     useEffect(()=>{
-        document.title=`${value} ${data}`
-    },[value,data])
+        document.title=`${value} ${data}`;
+        let a=document.getElementById('btn2');
+        a.addEventListener('click',()=>{
+            document.body.style.backgroundColor="black"
+        })
+    },[])
   return (
     <div>
         <h1>{value}{data}</h1>
         <input type="text" onChange={(e)=>setData(e.target.value)} />
         <button id="btn1" onClick={()=>setvalue(value+1)}>Value</button>
-
+        <button id="btn2" >Click</button>
     </div>
   )
 }
